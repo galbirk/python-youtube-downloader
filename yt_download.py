147817b5699a529912f0d3e109fc132c0faf67fa
Gal Birkman <galbirkman@gmail.com>
2020-12-31 00:07:58 +0200

    Added documentation

--- yt_download.py
+++ yt_download.py
@@ -6,6 +6,7 @@ import argparse
 import csv 
 import os
 
+# Function to convert and dwonload youtube videos
 def convert_download(url):
     driver.get("https://ytmp3.cc/en9/")
     url_input = driver.find_element_by_id('input')
@@ -21,6 +22,7 @@ def convert_download(url):
 chromedriver = r"{}\chromedriver.exe".format(os.path.dirname(os.path.realpath(__file__)))
 downloads_path = r"{}\downloads".format(os.path.dirname(os.path.realpath(__file__)))
 
+# Arguments Parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-u','--url',dest='url',help="url from youtube to be Downloaded")
 parser.add_argument('-b','--bin-location',dest='bin',help="Path for the .exe file of chrome",default=r"C:\Program Files\Google\Chrome\Application\chrome.exe")
@@ -29,6 +31,7 @@ parser.add_argument('-c','--chrome-driver',dest='chromedriver',help="Path to chr
 parser.add_argument('-d','--download-path',dest='downloads_path',help="Path for files to be dowloaded to",default=downloads_path)
 args = parser.parse_args()
 
+# Arguments validation
 if not args.csv and not args.url:
     raise parser.error('Must Enter url or csv path')
 elif args.csv and args.url:
@@ -37,12 +40,14 @@ elif args.csv and args.url:
 if not os.path.exists(downloads_path):
     os.mkdir(downloads_path)
 
+# Creates the chrome driver with custom options
 options = webdriver.ChromeOptions() 
 prefs = {"download.default_directory" : args.downloads_path}
 options.add_experimental_option("prefs",prefs)
 options.binary_location = args.bin
 driver = webdriver.Chrome(options=options, executable_path=args.chromedriver, )
 
+# Check if csv path was entered
 if args.csv:
     with open(f'{args.csv}',newline='') as csv_file:
         reader = csv.reader(csv_file)
